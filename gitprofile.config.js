3d5bcf8feb871e48164b7ce12396946118aad9a6
Iago Calazans <iago.calazans@gmail.com>
2024-05-07 07:31:02 -0300

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -132,11 +132,11 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'dracula',
+    defaultTheme: 'wireframe',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
@@ -147,8 +147,7 @@ const config = {
 
     // Available themes. To remove any theme, exclude from here.
     themes: [
-      'dracula',
-      'acid',
+      'wireframe',
     ],
 
     // Custom theme
